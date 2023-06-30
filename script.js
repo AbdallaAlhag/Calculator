251bbeedcc50600525c01af3c3ebf2f173406077
Abdalla Alhag <alhagabdalla@gmail.com>
2023-06-30 09:55:50 -0700

    intial project finished, added decimals, fixed some bugs

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 let variable1 = 0;
 let variable2 = 0;
 let operation = "";
+let isNewNumber = false;
+let isEqual = false;
+let isDot = false;
 
 // dont know how to use objects with bottons
-
-let user1 = {
-    name: 'john',
-    age: 30
-};
 
 let currentScreen = document.querySelector('.screen-current');
 let prevScreen = document.querySelector('.screen-previous');
 const buttons = document.querySelectorAll('button');
 const clear = document.getElementById('clear');
 const result = document.getElementById('equal');
-
+const negate = document.getElementById('negate');
+const decimal = document.getElementById('decimal');
 
 // MAYBE USELESS???
 // const addBtn = document.getElementById('addition');
@@ -22,26 +21,63 @@ const result = document.getElementById('equal');
 // const multBtn = document.getElementById('multiplication');
 // const divBtn = document.getElementById('division'); 
 
+// const calculator = {
+//     currentValue: 0,
+
+//     add(num) {
+//         this.currentValue += num;
+//         return this;
+//     },
+
+//     subtract(num) {
+//         this.currentValue -= num;
+//         return this;
+//     },
+
+//     multiply(num) {
+//         this.currentValue *= num;
+//         return this;
+//     },
+
+//     divide(num) {
+//         this.currentValue /= num;
+//         return this;
+//     },
+
+//     clear() {
+//         this.currentValue = 0;
+//         return this;
+//     },
+
+//     getResult() {
+//         return this.currentValue;
+//     }
+// }
+
+// calculator.add(5); // Add 5 to the current value
+// calculator.multiply(3); // Multiply the current value by 3
+// calculator.subtract(2); // Subtract 2 from the current value
+// calculator.divide(2); // Divide the current value by 2
+// const result = calculator.getResult(); // Get the final result
+// console.log(result); // Output: 7
 
 // First sequence
-
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        // alert(button.id);
-        // if(button.first)
-        if(button.firstElementChild.className === 'number') {
-            // return;
-            currentScreen.textContent += parseInt(button.textContent);
-            variable1 = parseInt(currentScreen.textContent);
-        }
-        // Doesn't wrap yet
         
-        // This is coming out wrong
-        // variable1 += button.textContent; 
-        // console.log(variable1);
+        if(button.firstElementChild.className === 'number' && currentScreen.textContent.length < 22) {
+            // return;
+            if (isNewNumber === true) {
+                currentScreen.textContent = ' ';
+                isNewNumber = false;
+                isDot = false;
+            }
+            currentScreen.textContent += parseFloat(button.textContent);
+            variable1 = parseFloat(currentScreen.textContent);
+        }
+        
     })
 })
-
 
 // buttons.forEach(button => console.log(button));
 
@@ -49,39 +85,49 @@ buttons.forEach((button) => {
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         if(button.firstElementChild.className === 'op') {
-            prevScreen.textContent = currentScreen.textContent + " " + button.textContent;
-            variable2 = parseInt(prevScreen.textContent);
+            prevScreen.textContent = variable1 + " " + button.textContent;
+            variable2 = parseFloat(prevScreen.textContent);
             operation = button.id;
-            console.log(operation);
             currentScreen.textContent = ' ';
+            isEqual = false;
+            isDot = false;
         }
     })
 })
 
-
-// Third sequence 
+// Third sequence equal button clicked
 result.addEventListener('click', () =>{
+    if(isEqual === true) {
+        return;
+    }
     prevScreen.textContent += variable1 + ' ' + "=";
-    console.log(prevScreen.textContent);
-    currentScreen.textContent = operatorFunc(operation,variable2, variable1);
-    console.log(currentScreen.textContent);
-})
+    currentScreen.textContent = operatorFunc(operation, variable2, variable1);
+    variable1 = parseFloat(currentScreen.textContent);
+    isNewNumber = true;
+    isEqual = true;
+});
 
+// dot button
+decimal.addEventListener('click', () => {
+    if(isDot === true) {
+        return;
+    }
+    currentScreen.textContent += ".";
+    variable1 = parseFloat(currentScreen.textContent);
+    isDot = true;
+});
 
-function test(){
-    variable1 = parseInt(currentScreen.textContent);
-    console.log(variable1);
-};
-
-function answer() {
-    console.log(variable1 + variable2);
-}
+negate.addEventListener('click', () =>{
+    variable1 *= -1;
+    currentScreen.textContent = variable1;
+});
 
 clear.addEventListener('click', () => {
     currentScreen.textContent = ' ';
     prevScreen.textContent = ' ';
-})
-
+    variable1 = 0;
+    variable2 = 0;
+});
 
 const add = function(a,b) {
     return (a + b);
@@ -104,7 +150,9 @@ const divide = function(a,b) {
     }
 }
 
-
+const modular = function(a,b) {
+    return (a % b);
+}
 
 function operatorFunc(op, a, b) {
     switch(op) {
@@ -120,13 +168,15 @@ function operatorFunc(op, a, b) {
         case "division":
             return divide(a,b);
             break;
+        case "modular":
+            return modular(a,b);
+            break;
         default:
             ("Unknown");
     }
 }
 
+// console.log("hello: " + operatorFunc("subtraction", 10, 6));
 
 
-console.log("hello: " + operatorFunc("subtraction", 10, 6));
-
-
+console.log("test: " + parseFloat("123.5"));
